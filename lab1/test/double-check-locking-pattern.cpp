e736747f4b9d06868e276417f6d1f421e3699f70
Leedehai <hhlisu@yahoo.com>
2018-05-10 14:46:44 -0700

    Add another data race attempt

--- lab1/test/double-check-locking-pattern.cpp
+++ lab1/test/double-check-locking-pattern.cpp
@@ -5,17 +5,33 @@
 #include <thread>
 #include <vector>
 
-std::atomic<int> x(0), y(0);
+// std::atomic<int> x(0), y(0);
+// void GreenThreadWorker1(void *) {
+// 	printf("Green thread on kernel thread.\n");
+// 	if (x == 1) { y.fetch_add(1, std::memory_order_relaxed); }
+// 	printf("y = %d\n", y.load());
+// }
+
+// void GreenThreadWorker2(void *) {
+// 	if (y == 1) { x.fetch_add(1, std::memory_order_relaxed); }
+// 	printf("x = %d\n", x.load());
+// }
+
+int data  = 0, ready = 0;
+
+void use(int data) {
+  printf("use(data) prints %d, should've been 2000 if no data race\n", data);
+}
 
 void GreenThreadWorker1(void *) {
 	printf("Green thread on kernel thread.\n");
-	if (x == 1) { y.fetch_add(1, std::memory_order_relaxed); }
-	printf("y = %d\n", y.load());
+	data = 2000;
+  ready = 1;
 }
 
 void GreenThreadWorker2(void *) {
-	if (y == 1) { x.fetch_add(1, std::memory_order_relaxed); }
-	printf("x = %d\n", x.load());
+	while (!ready) {}
+  use(data);
 }
 
 void KernelThreadWorker(int n) {
